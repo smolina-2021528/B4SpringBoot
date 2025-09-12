ba4f3a0c88f8a0000e0f3d0797b33d3000555c34
smolina-2021528 <smolina-2021528@kinal.edu.gt>
2025-09-11 20:42:03 -0600

    feat: se agrego el segundo proyecto, con su estructura creada

---
+++ mechanicalshop/src/main/java/com/sebastianmolina/mechanicalshop/MechanicalShopApplication.java
@@ -0,0 +1,13 @@
+package com.sebastianmolina.mechanicalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicalShopApplication.class, args);
+	}
+
+}
---
+++ mechanicalshop/src/test/java/com/sebastianmolina/mechanicalshop/MechanicalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianmolina.mechanicalshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicalShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
